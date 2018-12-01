1448e1b3f595be41afa34769be0d0f20d3c6b12b
Kevin Prehn <kprehn01@gmail.com>
2018-12-01 12:24:18 -0600

    more formatting code

--- reddit_bot.py
+++ reddit_bot.py
@@ -5,15 +5,23 @@ import os
 
 comments_to_search = 5
 
-# Given comment text, can/should the bot reply to it?
-def can_reply_to_comment(text):
-	if "asdfjsdfjsdlkfjlsdkaflkdsf" in text:
-		return True
-	return False
-	
-def process_reply(text):
-	return "test"
+DO_NOT_RESPOND = 0
+TEST_COMMENT = 1
 
+# Given comment text, can/should the bot reply to it?
+def get_comment_response_code(comment):
+	if "this is for testing" in comment.body:
+		return TEST_COMMENT
+	return DO_NOT_RESPOND
+
+	
+def generate_comment_reply(comment, responsecode):
+	if responsecode == TEST_COMMENT:
+		return "Test comment"
+	return "Invalid Response Code"
+
+
+	
 def bot_login():
 	print ("Logging in...")
 	r = praw.Reddit('replybot', user_agent='TrumpReplyBot user agent')
@@ -25,15 +33,12 @@ def run_bot(r, comments_replied_to):
 	print ("Searching last " + str(comments_to_search) + " comments")
 
 	for comment in r.subreddit('test').comments(limit=comments_to_search):
-		if can_reply_to_comment(comment.body) and comment.id not in comments_replied_to and comment.author != r.user.me():
-			print (comment)
-			print(type(comment))
-			print(comment)
-			comment.reply(process_reply(comment.body))
+		comment_response_code = get_comment_response_code(comment)
+		if not comment_response_code == DO_NOT_RESPOND and comment.id not in comments_replied_to and comment.author != r.user.me():
+			resp = generate_comment_reply(comment, comment_response_code)
+			comment.reply(resp)
 			print ("Replied to comment " + comment.id)
-
 			comments_replied_to.append(comment.id)
-
 			with open ("comments_replied_to.txt", "a") as f:
 				f.write(comment.id + "\n")
 
